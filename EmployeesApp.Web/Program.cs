a5432643bc31ee0221e69f19bc464e737c12cbed
Iryna <iryna.chorna@hotmail.com>
2025-05-19 14:05:54 +0200

    Added jQuery

--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -1,14 +1,14 @@
-namespace EmployeesApp.Web
+namespace EmployeesApp.Web;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllersWithViews();
-            var app = builder.Build();
-            app.MapControllers();
-            app.Run();
-        }
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        var app = builder.Build();
+        app.MapControllers();
+        app.UseStaticFiles();
+        app.Run();
     }
 }
